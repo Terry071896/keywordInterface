a501035728ef2f7f0aca1cc517940b21b11431b1
Terry Cox <tcox@carroll.edu>
2019-09-23 17:21:38 -1000

    first commit for keywords

---
+++ keywords.py
@@ -0,0 +1,43 @@
+
+class Keywords(object):
+
+    def __init__(self, servers=None, keywords=None, mode='web'):
+
+        if keywords is None:
+            keywords = []
+        if servers is None:
+        	servers = []
+		
+		
+		self._servers = servers
+        self._keywords = keywords
+        self._mode = mode
+
+    def get_keyword(self):
+    	counter = 0
+    	keywordDict = dict()
+    	for pname in self._keywords:
+    		server = self._severs[counter]
+    		keywordDict.update({pname : self._find_keyword(server, pname)})
+    
+        return keywordDict
+
+    def _find_keyword(self, server, keyword):
+		if self._mode == 'local':
+			proc = subprocess.Popen("show -terse -s %s %s " % (server, keyword), stdout=subprocess.PIPE, shell=True)
+			result = proc.communicate()
+		elif self._mode == 'ktlpython':
+			proc = ktl.cache(server, keyword)
+			result = proc.read()
+		elif self._mode == 'web':
+			url = 'http://vm-kcwi:5002/show/%s/%s' % (server, keyword)
+			try:
+				response = requests.get(url)
+				#print(response.json())
+			except requests.exceptions.RequestException as e:
+				print("Error in getting data from the server")
+				return
+			result = response.json()
+		elif self._mode == 'simulate':
+			return 164
+		return result
